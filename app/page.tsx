26c4d12d9ef6019a9a0eeaa10c9bd5a4ac09d717
vivek3410 <87080539+vivek3410@users.noreply.github.com>
2024-05-15 16:01:27 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Navbarhome from "@/components/Navbarhome";
 export default function Home() {
   return (
     <>
-    <Navbarhome/>
-    <main className="flex  flex-col items-center justify-center h-screen w-[100vw] bg-sky-400">
-      
-    </main>
+      <Navbarhome />
+      <main className="flex  flex-col items-center justify-center h-screen w-[100vw] bg-sky-400">
+        hello
+      </main>
     </>
   )
 }
